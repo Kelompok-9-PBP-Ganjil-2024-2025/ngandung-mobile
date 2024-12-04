060aff0464db7e0b9f8f5d33fa5e264244b950f3
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2024-12-04 14:51:04 +0700

    Navbar - Navbar untuk halaman forum sudah bisa

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'landing/home_screen.dart'; // HomeScreen from landing/screens
-import 'rating/rating_screen.dart'; // Rating screen from rating/screens
-import 'poll/poll_screen.dart'; // Poll screen from poll/screens
-import 'favorite/store_screen.dart'; // Store (Toko Fav) from favorite/screens
-import 'forum/forum_screen.dart'; // Forum screen from forum/screens
+import 'landing/home_screen.dart';    // HomeScreen from landing/screens
+import 'rating/rating_screen.dart';            // Rating screen from rating/screens
+import 'poll/poll_screen.dart';                // Poll screen from poll/screens
+import 'favorite/store_screen.dart';          // Store (Toko Fav) from favorite/screens
+import 'forum/forum_screen.dart';             // Forum screen from forum/screens
 
 void main() {
   runApp(const MyApp());
@@ -36,10 +36,11 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    const HomeScreen(),
-    const RatingScreen(),
-    const PollScreen(),
+    const HomeScreen(),    
+    const RatingScreen(),  
+    const PollScreen(),    
     const StoreScreen(),
+    const ForumScreen(),   
   ];
 
   void _onItemTapped(int index) {
@@ -59,31 +60,35 @@ class _MyHomePageState extends State<MyHomePage> {
         child: _pages[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor:
-            const Color(0xFF111111), // Set background color to #111111
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: const Color(0xFFFEC123),
-        unselectedItemColor: const Color(0xFF111111),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: 'Rating',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.poll),
-            label: 'Polling',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.thumb_up),
-            label: 'Favorite',
-          ),
-        ],
-      ),
+  backgroundColor: const Color(0xFF111111), // Set background color to #111111
+  currentIndex: _selectedIndex,
+  onTap: _onItemTapped,
+  selectedItemColor: const Color(0xFFFEC123),
+  unselectedItemColor: const Color(0xFFEBEBEB), 
+  items: const [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.star),
+      label: 'Rating',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.poll),
+      label: 'Polling',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.thumb_up),
+      label: 'Favorite',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.message),
+      label: 'Comment',
+    ),
+  ],
+),
+
     );
   }
 }
