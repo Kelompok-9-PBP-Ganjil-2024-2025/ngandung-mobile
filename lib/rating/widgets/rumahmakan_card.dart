bedeb4afdbeb50f008da6f177e97c0def324a6a5
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-12-21 13:14:19 +0700

    feat: update RumahMakanCard to include navigation to RatingPage and enhance layout with rounded corners

--- lib/rating/widgets/rumahmakan_card.dart
+++ lib/rating/widgets/rumahmakan_card.dart
@@ -1,6 +1,6 @@
-// /lib/rating/widgets/rumahmakan_card.dart
 import 'package:flutter/material.dart';
 import '../models/rumahmakan.dart';
+import '../screens/rating_view.dart';
 
 class RumahMakanCard extends StatelessWidget {
   final RumahMakan rumahMakan;
@@ -15,55 +15,73 @@ class RumahMakanCard extends StatelessWidget {
     final fields = rumahMakan.fields;
     return Card(
       elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-          crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-          children: [
-            Text(
-              fields.namaRumahMakan,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0), // Match the border radius
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12.0), // Apply the same radius here
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => RatingPage(id: rumahMakan.pk),
             ),
-            const SizedBox(height: 8),
-            Text(
-              fields.alamat,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Rating: ${fields.averageRating}',
-                  style: const TextStyle(fontWeight: FontWeight.w500),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+            crossAxisAlignment:
+                CrossAxisAlignment.center, // Center horizontally
+            children: [
+              Text(
+                fields.namaRumahMakan,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                const SizedBox(width: 4),
-                const Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 16,
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Reviewed by ${fields.numberOfRatings}',
-                  style: const TextStyle(color: Colors.grey),
-                ),
-                const SizedBox(width: 4),
-                const Icon(
-                  Icons.person,
-                  color: Colors.grey,
-                  size: 16,
-                ),
-              ],
-            ),
-          ],
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                fields.alamat,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Rating: ${fields.averageRating}',
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(width: 4),
+                  const Icon(
+                    Icons.star,
+                    color: Colors.yellow,
+                    size: 16,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Reviewed by ${fields.numberOfRatings}',
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                  const SizedBox(width: 4),
+                  const Icon(
+                    Icons.person,
+                    color: Colors.grey,
+                    size: 16,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
